7fc8c3f0a1970dc1166d7a761a9eb5d39ebbb1b1
Mike K <rocker@gazeta.pl>
2026-07-08 15:50:09 +0200

    Write and read payload to file

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SharpYaml;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 
 namespace AutoSkippy.ViewModels;
@@ -10,25 +13,37 @@ public partial class MainWindowViewModel : ViewModelBase
     public static string VersionNumber => Assembly.GetExecutingAssembly()?.GetName().Version?.ToString() ?? "Unknown";
 
     [ObservableProperty]
-    private ScpiPayload? _currentPayload;
+    private ScpiPayload _currentPayload = new();
 
     [ObservableProperty]
     private string _currentPayloadPath = string.Empty;
 
     public MainWindowViewModel()
     {
-        CurrentPayload = new() { LoopCount = 10, SetupLines = "SETUP", LoopLines = "LOOP", TeardownLines = "TEARDOWN" };
     }
 
-    public static void SavePayloadToYaml(ScpiPayload payload)
+    public static async void SavePayloadToYaml(ScpiPayload payload, string fullFileName)
     {
         var yaml = YamlSerializer.Serialize(payload);
+        try
+        {
+            using var sw = new StreamWriter(fullFileName, false) { AutoFlush = true };
+            await sw.WriteAsync(yaml);
+            sw.Close();
+            sw.Dispose();
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e.ToString());
+        }
     }
 
     [RelayCommand]
     public void SavePayload()
     {
-        if (CurrentPayload is ScpiPayload p)
-        SavePayloadToYaml(p);
+        if (CurrentPayload is ScpiPayload p && !string.IsNullOrEmpty(CurrentPayloadPath))
+        {
+            SavePayloadToYaml(p, CurrentPayloadPath);
+        }
     }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,12 +1,29 @@
 using AutoSkippy.ViewModels;
 using Avalonia.Controls;
 using Avalonia.Platform.Storage;
+using SharpYaml;
+using SharpYaml.Model;
+using System;
+using System.Diagnostics;
 using System.IO;
+using System.Text.Json;
 
 namespace AutoSkippy.Views;
 
 public partial class MainWindow : Window
 {
+    private YamlSerializerOptions _yamlSerializerOptions = new YamlSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = true,
+        IndentSize = 4,
+        DefaultIgnoreCondition = YamlIgnoreCondition.WhenWritingNull,
+        DuplicateKeyHandling = YamlDuplicateKeyHandling.FirstWins,
+        MappingOrder = YamlMappingOrderPolicy.Declaration,
+        UnmappedMemberHandling = System.Text.Json.Serialization.JsonUnmappedMemberHandling.Skip,
+        PropertyNameCaseInsensitive = true,
+    };
+
     public static FilePickerFileType SCPIPayload { get; } = new("SCPI Payload")
     {
         Patterns = [ "*.yaml" ],
@@ -35,7 +52,22 @@ public partial class MainWindow : Window
 
         if (files.Count != 1) return;
 
+        vm.CurrentPayload = new();
         vm.CurrentPayloadPath = files[0].Path.LocalPath;
+        try
+        {
+            using StreamReader sr = new(vm.CurrentPayloadPath);
+            var yaml = YamlStream.Load(sr);
+            var pld = YamlSerializer.Deserialize<ScpiPayload>(yaml.ToString(), _yamlSerializerOptions);
+            if (pld is not null)
+            {
+                vm.CurrentPayload = pld;
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.ToString());
+        }
     }
 
     private async void ButtonSaveAsClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
@@ -49,16 +81,16 @@ public partial class MainWindow : Window
             Title = "Open SCPI payload",
             DefaultExtension = "yaml",
             ShowOverwritePrompt = true,
-            SuggestedFileName = Path.GetFileName(vm.CurrentPayloadPath),
+            SuggestedFileName = System.IO.Path.GetFileName(vm.CurrentPayloadPath),
             SuggestedStartLocation = await StorageProvider.TryGetWellKnownFolderAsync(WellKnownFolder.Documents),
             SuggestedFileType = SCPIPayload,
         });
 
         if (file is null) return;
 
-        if (vm.CurrentPayload is ScpiPayload p)
+        if (file.TryGetLocalPath() is string path)
         { 
-            MainWindowViewModel.SavePayloadToYaml(p); 
+            MainWindowViewModel.SavePayloadToYaml(vm.CurrentPayload, path); 
         }
     }
 }
