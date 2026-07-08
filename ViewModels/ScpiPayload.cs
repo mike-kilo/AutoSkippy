6b439c27c4f1e07ee2840b697009a53be3535a77
Mike K <rocker@gazeta.pl>
2026-07-08 14:02:42 +0200

    Add SCPI payload structure

---
+++ ViewModels/ScpiPayload.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AutoSkippy.ViewModels;
+
+public partial class ScpiPayload : ViewModelBase
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(StepsCount))]
+    private string _setupLines = string.Empty;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(StepsCount))]
+    private string _loopLines = string.Empty;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(StepsCount))]
+    private int _loopCount = 0;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(StepsCount))]
+    private string _teardownLines = string.Empty;
+
+    public int StepsCount => 
+        SetupLines.Split("{Environment.NewLine}").Length + 
+        LoopCount * LoopLines.Split("{Environment.NewLine}").Length +
+        TeardownLines.Split("{Environment.NewLine}").Length;
+}
